0c17d34b7087acc22753f1990bafb816d47e72cc
Pelli <martinpelli@outlook.com>
2023-04-17 14:48:10 -0300

    migrated to mapbox

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_maps_app/blocs/blocs.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_maps_app/themes/themes.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
 class MapView extends StatelessWidget {
   final LatLng initialPosition;
-  final Set<Polyline> polylines;
 
-  const MapView({Key? key, required this.initialPosition, required this.polylines}) : super(key: key);
+  const MapView({Key? key, required this.initialPosition}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final mapBloc = BlocProvider.of<MapBloc>(context);
-
     final CameraPosition initialCameraPosition = CameraPosition(target: initialPosition, zoom: 17);
     final size = MediaQuery.of(context).size;
 
     return SizedBox(
         width: size.width,
         height: size.height,
-        child: Listener(
-          onPointerMove: (event) => mapBloc.add(OnStopFollowingUserEvent()),
-          child: GoogleMap(
-            onMapCreated: (controller) => mapBloc.add(OnMapInitilizedEvent(controller)),
-            initialCameraPosition: initialCameraPosition,
-            polylines: polylines,
-            zoomControlsEnabled: false,
-            myLocationButtonEnabled: false,
-            onCameraMove: (position) => mapBloc.mapCenter = position.target,
-          ),
-        ));
+        child: MapboxMap(styleString: "mapbox://styles/mapbox/navigation-night-v1", initialCameraPosition: initialCameraPosition, accessToken: ""));
   }
 }
